fa4e23a158377c1e5f597376e1e6b7ba7ed36858
HridyaSunil <83941466+HridyaSunil@users.noreply.github.com>
2021-05-11 15:49:05 +0530

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
+ class Hello {
+=======
 public class Hello {
+>>>>>>> 4b5c73eddae1fe276b43bc96c7e485caaa200c0d
 	public static void main(String[] args) {
 		
 		System.out.println("hai good morning!!!");
